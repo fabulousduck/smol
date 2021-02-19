c8115de159e7b4a6957252d415e27217673f3d02
fabulousduck <ryanvlaming@icloud.com>
2021-02-19 18:59:03 +0100

    remove repl and interpreter

--- cmd/smol/main.go
+++ cmd/smol/main.go
@@ -4,21 +4,13 @@ import (
 	"flag"
 
 	"github.com/fabulousduck/smol"
-	"github.com/fabulousduck/smol/repl"
 )
 
 func main() {
 	s := smol.NewSmol()
-	flagPtr := flag.Bool("c", false, "compile to a chip-8 rom")
 	filenamePtr := flag.String("file", "", "input file for the interpreter")
 
 	flag.Parse()
-
-	if *filenamePtr != "" {
-		s.RunFile(*filenamePtr, *flagPtr)
-	} else {
-		//TODO add on the fly rom compilation to repls
-		repl.Repl(s)
-	}
+	s.RunFile(*filenamePtr)
 
 }
--- smol.go
+++ smol.go
@@ -7,7 +7,6 @@ import (
 	"github.com/fabulousduck/smol/bytecode"
 
 	"github.com/fabulousduck/smol/ast"
-	"github.com/fabulousduck/smol/interpreter"
 	"github.com/fabulousduck/smol/ir"
 	"github.com/fabulousduck/smol/lexer"
 )
@@ -24,51 +23,28 @@ func NewSmol() *Smol {
 }
 
 //RunFile : Interprets a given file
-func (smol *Smol) RunFile(filename string, compile bool) {
+func (smol *Smol) RunFile(filename string) {
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
-	smol.Run(string(file), filename, compile)
+	smol.Run(string(file), filename)
 	if smol.HadError {
 		os.Exit(65)
 	}
 }
 
 //Run exectues a given script
-func (smol *Smol) Run(sourceCode string, filename string, compile bool) {
+func (smol *Smol) Run(sourceCode string, filename string) {
 	l := lexer.NewLexer(filename, sourceCode)
 	l.Lex()
 	p := ast.NewParser(filename, l.Tokens)
 	//We can ignore the second return value here as it is the amount of tokens consumed.
 	//We do not need this here
 	p.Ast, _ = p.Parse("")
-
-	if compile {
-		g := ir.NewGenerator(filename)
-		g.Generate(p.Ast)
-		bg := bytecode.Init(g, filename)
-		bg.CreateRom()
-		return
-	}
-	i := interpreter.NewInterpreter()
-	i.Interpret(p.Ast)
-}
-
-//RunRepl is the same as Run except it allows you to pass our own interpreter so we can keep the state
-//of it after the line has been executed
-func (smol *Smol) RunRepl(sourceCode string, filename string, statefullInterpreter *interpreter.Interpreter) {
-	l := lexer.NewLexer(filename, sourceCode)
-	l.Lex()
-	p := ast.NewParser(filename, l.Tokens)
-
-	//Add an EOF token so semicolon errors dont index out of range
-	p.Tokens = append(l.Tokens, lexer.Token{
-		Value: "end_of_file",
-		Type:  "end_of_file",
-	})
-	//We can ignore the second return value here as it is the amount of tokens consumed.
-	//We do not need this here
-	p.Ast, _ = p.Parse("")
-	statefullInterpreter.Interpret(p.Ast)
+	g := ir.NewGenerator(filename)
+	g.Generate(p.Ast)
+	bg := bytecode.Init(g, filename)
+	bg.CreateRom()
+	return
 }
